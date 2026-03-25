67ad3f9605f658475083720dedfe052ad37fe327
jeremy <jeremylecoder@gmail.com>
2026-03-25 23:46:12 +0300

    Added routes for auth pages

--- landing/views.py
+++ landing/views.py
@@ -2,3 +2,8 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'landing/index.html')
+def login_view(request):
+    return render(request, 'landing/login.html')
+def signup_view(request):
+    return render(request, 'landing/signup.html')
+
