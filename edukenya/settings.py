7767a56ec02418039247fc1bcfe1d4261e6e12da
jeremy <jeremylecoder@gmail.com>
2026-03-28 00:11:01 +0300

    Update settings.py

--- edukenya/settings.py
+++ edukenya/settings.py
@@ -1,14 +1,20 @@
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-SECRET_KEY = 'dev-secret-key'
+
+SECRET_KEY = 'dev-secret-key-change-in-production'
 DEBUG = True
 ALLOWED_HOSTS = ['*']
 ROOT_URLCONF = 'edukenya.urls'
 
 INSTALLED_APPS = [
+    'django.contrib.contenttypes',
     'django.contrib.staticfiles',
+    'django.contrib.auth',
+    'django.contrib.sessions',
+    'django.contrib.messages',
     'landing',
+    'accounts',
 ]
 
 TEMPLATES = [{
@@ -18,8 +24,45 @@ TEMPLATES = [{
     'OPTIONS': {
         'context_processors': [
             'django.template.context_processors.request',
+            'django.contrib.auth.context_processors.auth',
+            'django.contrib.messages.context_processors.messages',
         ],
     },
 }]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+]
+
+DATABASES = {
+    'default': {
+        'ENGINE':   'django.db.backends.mysql',
+        'NAME':     'edukenya_db',
+        'USER':     'root',
+        'PASSWORD': '',
+        'HOST':     '127.0.0.1',
+        'PORT':     '3306',
+        'OPTIONS':  {'charset': 'utf8mb4'},
+    }
+}
+
+
+
+EMAIL_BACKEND       = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST          = 'smtp-relay.brevo.com'
+EMAIL_PORT          = 587
+EMAIL_USE_TLS       = True
+EMAIL_HOST_USER     = ''       
+EMAIL_HOST_PASSWORD = ''   
+DEFAULT_FROM_EMAIL  = ''
+
+AUTH_USER_MODEL = 'accounts.User'
+
+SESSION_ENGINE = 'django.contrib.sessions.backends.db'
 
 STATIC_URL = '/static/'
